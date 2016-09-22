0d3d423e695c012c075ea10d5b5fed4a2cdab345
Drew Tyler <drew@drewtyler.com>
2016-09-21 19:16:36 -0700

    Mongo

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ var bot = new Discord.Client();
 var AuthDetails = require("./auth.json");
 var Config = require("./config.json");
 var MongoClient = require('mongodb').MongoClient;
-var url = AuthDetails.mongoUrl;
+var mongoUrl = AuthDetails.mongoUrl;
 var Commands = require('./commands.js');
 
 bot.on('ready', function() {
+    MongoClient.connect(mongoUrl, function(err, db) {
+        if(err) {
+            console.log(err);
+        } else {
+            
+        }
+    });
     console.log('I am ready');
     console.log(bot.user);
     console.log("Ready to begin! Serving in " + bot.channels + " channels");
